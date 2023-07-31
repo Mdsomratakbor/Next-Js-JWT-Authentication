d0ff6ff6362b6caf6845c0733c42fa9c8caee10d
Md Somrat Akbor <somrat876@gmail.com>
2023-07-31 22:49:59 +0600

    custom login page implement

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -45,6 +45,9 @@ const handler = NextAuth({
         maxAge: 30 * 24 * 60 * 60, // 30 days
         updateAge: 24 * 60 * 60, // 24 hours
         
+      },
+      pages:{
+        signIn: "/auth/signIn",
       }
 });
 
